ac305e1cd2dbdd19c1599437c5a1068597c7163c
benkarmi <69310049+benkarmi@users.noreply.github.com>
2025-11-04 11:58:23 +0200

    Completed HW

--- FVCalc.java
+++ FVCalc.java
@@ -2,11 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt(args[0]);
-		double annualRate = (double) Integer.parseInt(args[1]);
+		int annualRate = Integer.parseInt(args[1]);
 		int years = Integer.parseInt(args[2]);
+		double annualRate1 = (double) annualRate;
 
 		double rate = annualRate / 100.0;
 		int futureValue = (int) Math.round(currentValue * Math.pow(1 + rate, years));
-		System.out.println("After " + years + " years, " + "$" + currentValue + " saved at " + annualRate + "% will yield $" + futureValue);
+		System.out.println("After " + years + " years, " + "$" + currentValue + " saved at " + annualRate1 + "% will yield $" + futureValue);
 	}
 }
